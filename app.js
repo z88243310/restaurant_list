190d9f8f9ecef55898bd894acfbea0d19167be8e
z88243310 <z88243310@gmail.com>
2021-10-20 23:37:10 +0800

    Built static website with Handlebars

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // require packages used in the project
 const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
+// handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
 // routes setting
 app.get('/', (req, res) => {
-  res.send(`<h1>This is restaurant list</h1>`)
+  res.render('index')
 })
-
+app.get('/restaurants/:id', (req, res) => {
+  res.render('show')
+})
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`The server is listening on http://localhost:${port}`)
