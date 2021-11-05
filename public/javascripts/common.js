e569ecf34f1124702e9869e8a46d6c9dc7cabd35
z88243310 <z88243310@gmail.com>
2021-11-05 14:38:21 +0800

    feat: add delete button in edit and detail page

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,8 +1,22 @@
 const formDelete = document.querySelectorAll('#form-delete')
-
 formDelete.forEach((form) => {
+  console.log('form')
   form.addEventListener('submit', function onFormSubmitted(event) {
     event.preventDefault()
     if (confirm('確定刪除嗎？')) event.target.submit()
+  })
+})
+
+const btnDelete = document.querySelectorAll('#btn-delete')
+btnDelete.forEach((btn) => {
+  btn.addEventListener('click', function onButtonSubmitted(event) {
+    if (confirm('確定刪除嗎？')) {
+      const formDelete = document.createElement('form')
+      const id = event.target.dataset.id
+      formDelete.method = 'post'
+      formDelete.action = `/restaurants/${id}?_method=DELETE`
+      document.body.appendChild(formDelete)
+      formDelete.submit()
+    }
   })
 })
