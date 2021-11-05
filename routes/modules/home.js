e84034e2eaef9e11db09a74f122c7b8a57fc5c2b
z88243310 <z88243310@gmail.com>
2021-11-05 13:21:01 +0800

    feat: update search router with sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,6 +8,7 @@ const Restaurant = require('../../models/restaurant')
 router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
+    .sort({ name: 'asc' })
     .then((restaurants) => res.render('index', { restaurants }))
     .catch((error) => console.log(error))
 })
@@ -15,11 +16,30 @@ router.get('/', (req, res) => {
 // queryString : search
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword.toLowerCase().trim()
+  const modeSelected = req.query.modeSelected
+  let modeStatus = ''
+  switch (modeSelected) {
+    case 'A->Z':
+      modeStatus = { name: 'asc' }
+      break
+    case 'Z->A':
+      modeStatus = { name: 'desc' }
+      break
+    case '類別':
+      modeStatus = { category: 'asc' }
+      break
+    case '地區':
+      modeStatus = { location: 'asc' }
+      break
+    default:
+      modeStatus = { name: 'asc' }
+      break
+  }
   return Restaurant.find({ $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })
     .lean()
-    .then((restaurants) => res.render('index', { restaurants, keyword }))
+    .sort(modeStatus)
+    .then((restaurants) => res.render('index', { restaurants, keyword, modeSelected }))
     .catch((error) => console.log(error))
-  // { restaurants : restaurants , keyword : keyword } object literal extension
 })
 
 // 匯出路由模組
