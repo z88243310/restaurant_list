c3f2151faf7cf444f1a1fdf6a916885f00c281c0
z88243310 <z88243310@gmail.com>
2021-12-06 23:30:06 +0800

    feat: add login and register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,4 +6,12 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {})
+
 module.exports = router
