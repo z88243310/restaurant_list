5c608d323914148569cbff38ca136d58fb49bdf2
z88243310 <z88243310@gmail.com>
2021-12-07 00:12:30 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -12,6 +14,28 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res) => {})
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  // 檢查是否已註冊
+  User.findOne({ email }).then((user) => {
+    // 已經註冊，返回註冊頁
+    if (user) {
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword,
+      })
+    }
+    // 尚未註冊，寫入資料庫
+    return User.create({
+      name,
+      email,
+      password,
+    })
+      .then(() => res.redirect('/'))
+      .catch((error) => console.log(error))
+  })
+})
 
 module.exports = router
