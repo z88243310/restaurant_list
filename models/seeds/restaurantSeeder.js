61f4f43cee9f31f83fff82a2e2cdc3f0566ba83b
z88243310 <z88243310@gmail.com>
2021-12-14 23:12:49 +0800

    feat: modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,12 +1,53 @@
+// require dotenv on OS
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose')
-const Restaurant = require('../restaurant') // 載入 todo model
-const restaurantSeed = require('./restaurant.json').results
+const Restaurant = require('../restaurant') // 載入 Restaurant model
+const User = require('../user') // 載入 User model
+
+// restaurant and user Seeds
+const restaurantSeeds = require('./restaurant.json').results
+const userSeeds = [
+  { name: 'user1', email: 'user1@example.com', password: '12345678', restaurantId: [1, 2, 3] },
+  { name: 'user2', email: 'user2@example.com', password: '12345678', restaurantId: [4, 5, 6] },
+]
 
 db.once('open', () => {
-  Restaurant.insertMany(restaurantSeed)
-    .catch(() => console.log(`${seed.name} is error!`))
+  const restaurantFiltered = []
+  Promise.all(
+    Array.from(userSeeds, (userSeed) => {
+      const { name, email, password } = userSeed
+      // 加密
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hash) =>
+          // 將 user 寫入資料庫
+          User.create({ name, email, password: hash }).then((user) => {
+            // 找出符合 user's restaurantId 的 restaurantSeeds 資料並設定 userId
+            userSeed.restaurantId.forEach((restaurantId) => {
+              const restaurant = restaurantSeeds.find((restaurantSeed) => restaurantSeed.id === restaurantId)
+              restaurant.userId = user._id
+              restaurantFiltered.push(restaurant)
+            })
+          })
+        )
+    })
+  )
+    .then(() =>
+      // 將 restaurantFiltered 寫入資料庫
+      Restaurant.insertMany(restaurantFiltered)
+    )
+    .catch((error) => console.log(error))
     .finally(() => {
       console.log('data is created!')
+      console.log('\n[ Seed Data ]\n')
+      userSeeds.forEach((userSeed) => {
+        console.log(userSeed.email, userSeed.password)
+      })
       process.exit()
     })
 })
