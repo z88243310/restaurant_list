bd5a77c6e54e8df983724f0d854215f2ad0c9fe1
z88243310 <z88243310@gmail.com>
2022-01-09 17:13:21 +0800

    modify email incorrect message

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
 const User = require('../models/user')
 
-module.exports = (app) => {
+module.exports = app => {
   // 初始化 Passport 模組
   app.use(passport.initialize())
   app.use(passport.session())
@@ -13,14 +13,14 @@ module.exports = (app) => {
   passport.use(
     new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
       User.findOne({ email })
-        .then((user) => {
+        .then(user => {
           // 使用者不存在
           if (!user) {
             req.flash('email', email)
-            return done(null, false, { message: 'That email is not registered!' })
+            return done(null, false, { message: 'Email or Password incorrect.' })
           }
           // 密碼比對
-          return bcrypt.compare(password, user.password).then((isMatch) => {
+          return bcrypt.compare(password, user.password).then(isMatch => {
             // 密碼錯誤
             if (!isMatch) {
               req.flash('email', email)
@@ -30,7 +30,7 @@ module.exports = (app) => {
             return done(null, user)
           })
         })
-        .catch((error) => done(error, false))
+        .catch(error => done(error, false))
     })
   )
 
@@ -41,25 +41,25 @@ module.exports = (app) => {
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
         callbackURL: process.env.FACEBOOK_CALLBACK,
-        profileFields: ['email', 'displayName'],
+        profileFields: ['email', 'displayName']
       },
       (accessToken, refreshToken, profile, done) => {
         const { name, email } = profile._json
-        User.findOne({ email }).then((user) => {
+        User.findOne({ email }).then(user => {
           if (user) return done(null, user)
           const randomPassword = Math.random().toString(36).slice(-8)
           bcrypt
             .genSalt(10)
-            .then((salt) => bcrypt.hash(randomPassword, salt))
-            .then((hash) =>
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash =>
               User.create({
                 name,
                 email,
-                password: hash,
+                password: hash
               })
             )
-            .then((user) => done(null, user))
-            .catch((err) => done(err, false))
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
         })
       }
     )
@@ -72,7 +72,7 @@ module.exports = (app) => {
   passport.deserializeUser((id, done) => {
     User.findById(id)
       .lean()
-      .then((user) => done(null, user))
-      .catch((err) => done(err, null))
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
   })
 }
