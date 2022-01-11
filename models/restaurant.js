60867ecec5a78badfecb19ca244e8a8a833c0e9f
z88243310 <z88243310@gmail.com>
2022-01-12 00:37:18 +0800

    feat: recovery custom rating item

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,52 +3,51 @@ const Schema = mongoose.Schema
 const RestaurantSchema = new Schema({
   id: {
     type: Number, // 資料型別是數字
-    required: false, // 這是個必填欄位
+    required: false // 這是個必填欄位
   },
   name: {
     type: String,
-    required: true,
+    required: true
   },
   name_en: {
     type: String,
-    required: true,
+    required: true
   },
   category: {
     type: String,
-    required: true,
+    required: true
   },
   image: {
     type: String,
-    required: true,
+    required: true
   },
   location: {
     type: String,
-    required: true,
+    required: true
   },
   phone: {
     type: String,
-    required: true,
+    required: true
   },
   google_map: {
     type: String,
-    required: true,
+    required: true
   },
   rating: {
     type: Number,
-    required: false,
-    default: '5',
+    required: true
   },
   description: {
     type: String,
-    required: true,
+    required: true
   },
   userId: {
     // 加入關聯設定
     type: Schema.Types.ObjectId,
     ref: 'User',
     index: true,
-    required: true,
-  },
+    required: true
+  }
 })
 
 module.exports = mongoose.model('restaurant', RestaurantSchema)
