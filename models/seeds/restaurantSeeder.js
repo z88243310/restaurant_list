3a08256249ca12adb971246cb9621646b44d7bd0
z88243310 <z88243310@gmail.com>
2022-01-12 02:47:58 +0800

    refactor: seeder return array to insertmany

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -12,40 +12,45 @@ const User = require('../user') // 載入 User model
 const restaurantSeeds = require('./restaurant.json').results
 const userSeeds = [
   { name: 'user1', email: 'user1@example.com', password: '12345678', restaurantId: [1, 2, 3] },
-  { name: 'user2', email: 'user2@example.com', password: '12345678', restaurantId: [4, 5, 6] },
+  { name: 'user2', email: 'user2@example.com', password: '12345678', restaurantId: [4, 5, 6] }
 ]
 
 db.once('open', () => {
-  const restaurantFiltered = []
   Promise.all(
-    Array.from(userSeeds, (userSeed) => {
+    Array.from(userSeeds, userSeed => {
+      const restaurantFiltered = []
       const { name, email, password } = userSeed
       // 加密
       return bcrypt
         .genSalt(10)
-        .then((salt) => bcrypt.hash(password, salt))
-        .then((hash) =>
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash =>
           // 將 user 寫入資料庫
-          User.create({ name, email, password: hash }).then((user) => {
+          User.create({ name, email, password: hash }).then(user => {
             // 找出符合 user's restaurantId 的 restaurantSeeds 資料並設定 userId
-            userSeed.restaurantId.forEach((restaurantId) => {
-              const restaurant = restaurantSeeds.find((restaurantSeed) => restaurantSeed.id === restaurantId)
+            userSeed.restaurantId.forEach(restaurantId => {
+              const restaurant = restaurantSeeds.find(restaurantSeed => restaurantSeed.id === restaurantId)
               restaurant.userId = user._id
               restaurantFiltered.push(restaurant)
             })
+            return restaurantFiltered
           })
         )
     })
   )
-    .then(() =>
-      // 將 restaurantFiltered 寫入資料庫
-      Restaurant.insertMany(restaurantFiltered)
-    )
-    .catch((error) => console.log(error))
+    .then(restaurantFiltereds => {
+      const restaurantFilteredFinal = []
+      restaurantFiltereds.forEach(restaurantFiltered => {
+        restaurantFilteredFinal.push(...restaurantFiltered)
+      })
+      // 將 restaurantFilteredFinal 寫入資料庫
+      return Restaurant.insertMany(restaurantFilteredFinal)
+    })
+    .catch(error => console.log(error))
     .finally(() => {
       console.log('data is created!')
       console.log('\n[ Seed Data ]\n')
-      userSeeds.forEach((userSeed) => {
+      userSeeds.forEach(userSeed => {
         console.log(userSeed.email, userSeed.password)
       })
       process.exit()
